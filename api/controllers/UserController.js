ebbd7c6a48e60a62f66ed8f5367c5f49320034cf
Riyad4516 <riyad.ahsan4516@gmail.com>
2023-02-25 20:48:13 +0600

    Tested controller

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -11,7 +11,7 @@ module.exports = {
 
     CreateUsers: async function(req, res, next){
         try{
-            const user = await User.create({...req.body});
+            const user = await User.create({...req.body}); // this is just a comment
             res.status(200).json({
                 status: "successful",
                 data: user
