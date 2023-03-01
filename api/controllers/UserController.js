ddfc0e3f10e92d313c1155d740e852db0b8b27ef
Riyad4516 <riyad.ahsan4516@gmail.com>
2023-03-02 00:38:55 +0600

    test commit 4

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -2,7 +2,7 @@ const User = require("../models/Associations").User;
 
 module.exports = {
     GetAllUsers : async function(req, res){
-        const user = await User.findAll({include: "user_profile"});
+        const user = await User.findAll({include: "user_profile"}); // another commit
         res.status(200).json({
             status: "Success",
             data: user
